26a4e56c64b0a344cc45f85a14af7805405d6fb7
Romain <giot.romain@gmail.com>
2016-02-09 22:48:58 +0100

    Initial import

---
+++ {{ cookiecutter.project_name_slug }}/src/test.asm
@@ -0,0 +1,32 @@
+    org 0x4000
+
+
+
+    macro INFINITE_LOOP
+        jp $
+    endm
+
+
+    ls hl, message
+
+.loop
+        ld a, (hl)
+        or a
+        jp .end
+        push hl
+            call PRINT_CHAR
+        pop hl
+        inc hl
+        jr .loop
+
+.end
+    ld a, 7
+    call PRINT_CHAR
+
+    INFINITE_LOOP
+
+
+message defs "Hello {{group_name}} !"
+    db 0
+
+PRINT_CHAR equ 0xBB5A
