e3e8098b37128e8d075d35790f4e04ab15b504d9
Romain Giot <romain.giot@u-bordeaux.fr>
2017-11-05 21:16:23 +0100

    Fix date parsing + allow container to use sound

--- {{ cookiecutter.project_name_slug }}/bootstrap.sh
+++ {{ cookiecutter.project_name_slug }}/bootstrap.sh
@@ -8,6 +8,8 @@ DOCKER_IMAGE={{cookiecutter.group_name_slug}}/{{cookiecutter.project_name_slug}}
 DSK_TEST=./{{cookiecutter.project_name_slug}}.dsk
 
 
+{% raw %}
+
 # OS detection code
 UNAME=$(uname -s)
 DETECTED_OS=linux
@@ -23,12 +25,14 @@ linux)
 	LOCAL_WORKING_DIRECTORY=$(pwd)
 	DOCKER_WORKING_DIRECTORY=/home/arnold/project
 	DOCKER_X11_FORWARDING="-e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged"
+	DOCKER_SOUND_CONFIGURATION="-v /dev/snd:/dev/snd -v /dev/shm:/dev/shm -v /etc/machine-id:/etc/machine-id -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse -v /var/lib/dbus:/var/lib/dbus -v $HOME/.pulse:/home/arnold/.pulse"
 	;;
 
 windows)
 	LOCAL_WORKING_DIRECTORY=/$(pwd)
 	DOCKER_WORKING_DIRECTORY=//home/arnold/project
 	DOCKER_X11_FORWARDING=""
+	DOCKER_SOUND_CONFIGURATION=""
 	;;
 esac
 
@@ -38,8 +42,16 @@ DOCKER_HOSTNAME=$(basename $(pwd))
 DOCKER_BASEIMAGE=$(grep FROM Dockerfile | sed -e 's/^FROM\s*//')
 
 # XXX Lines modified to be compatible with cookie cutter parsing
-DOCKER_IMAGE_DATE=$(docker images --format="\{\{.CreatedAt\}\}" $DOCKER_IMAGE)
-DOCKER_BASEIMAGE_DATE=$(docker images --format="\{\{.CreatedAt\}\}" $DOCKER_BASEIMAGE)
+DOCKER_IMAGE_DATE=$(docker images --format="{{.CreatedAt}}" $DOCKER_IMAGE)
+
+if [[ $DOCKER_BASEIMAGE == *:* ]]
+then
+DOCKER_BASEIMAGE_DATE=$(docker images --format="{{.CreatedAt}}" $DOCKER_BASEIMAGE)
+else
+DOCKER_BASEIMAGE_DATE=$(docker images --format="{{.CreatedAt}}" $DOCKER_BASEIMAGE:latest)
+fi
+
+echo $DOCKER_BASEIMAGE_DATE
 
 # XXX Attention here it is not portable at all
 # XXX Need to find a way which works everywhere
@@ -61,37 +73,38 @@ function launchImage
 
 
     # create the image if it does not exists
-    if test "$($DOCKER images -q $DOCKER_IMAGE 2> /dev/null)" = "" 
+    if test "$($DOCKER images -q $DOCKER_IMAGE 2> /dev/null)" = ""
     then
         buildImage
     fi
-	
+
 	# Manage parameters for aft
 	# XXX Implement a better and more robust way to do things ...
-	case $DETECTED_OS in 
+	case $DETECTED_OS in
 		linux) AFT_PORT=/dev/ttyUSB0 ;;
 		windows) AFT_PORT=/dev/ttyS3 ;;
 	esac
-	
+
     if  test -e "$AFT_PORT"
     then
-		AFT_OPTION="--device $AFT_PORT" 
+		AFT_OPTION="--device $AFT_PORT"
     else
 		echo "aft unusable ($AFT_PORT does not exists)" >&2
 		AFt_OPTION=""
 	fi
 
 
-    ALL_OPTIONS=" -h $DOCKER_HOSTNAME 
-	-v $LOCAL_WORKING_DIRECTORY:$DOCKER_WORKING_DIRECTORY 
-	-w $DOCKER_WORKING_DIRECTORY 
-	$DOCKER_X11_FORWARDING 
-	$DOCKER_AFT_OPTION 
+    ALL_OPTIONS=" -h $DOCKER_HOSTNAME
+	-v $LOCAL_WORKING_DIRECTORY:$DOCKER_WORKING_DIRECTORY
+	-w $DOCKER_WORKING_DIRECTORY
+	$DOCKER_X11_FORWARDING
+	$DOCKER_AFT_OPTION
+	$DOCKER_SOUND_CONFIGURATION
 	$AFT_OPTION
-	--rm=true 
-	-i 
-	-t 
-	$DOCKER_IMAGE 
+	--rm=true
+	-i
+	-t
+	$DOCKER_IMAGE
         $*"
 
 
@@ -165,5 +178,5 @@ case "$1" in
 esac
 
 
-
+{% endraw %}
 
